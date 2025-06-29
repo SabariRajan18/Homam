b39dc7d77a61c87e10beb30ddfb859b180ea5905
suryaprakashclarisco <suryaprakash@clariscosolutions.com>
2025-06-29 13:38:42 +0530

    add changes

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Rameswaram_Homam',
+      script: 'src/server.ts',
+      interpreter: 'ts-node',
+      watch: false,
+      instances: 1,
+      autorestart: true,
+      error_file: './src/pm2/err-0.log',
+      out_file: './src/pm2/out-0.log',   
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
