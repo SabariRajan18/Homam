148eacf7eae247dc0af34efe49c11aea1b859615
unknown <mohameda.apxis@gmail.com>
2025-06-30 18:08:32 +0530

    dashboard

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/Homam";
 
 export const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect("mongodb+srv://sabarirajanm1018:XXSWvGNCtx2Osp6W@homam.tndmy1a.mongodb.net/?retryWrites=true&w=majority&appName=Homam");
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error);
--- src/routes/adminRouter/intex.ts
+++ src/routes/adminRouter/intex.ts
@@ -8,10 +8,10 @@ const router = Router();
 router.get("/login", adminController.login);
 router.post("/login", adminController.adminLogin);
 
-router.get("/dashboard", adminAuth, adminController.dashboard);
-router.get("/admin-uploads", adminAuth, adminController.adminUploads);
-router.post("/upload-image", adminAuth, upload.single("image"), adminController.uploadImage);
-router.post("/delete-image/:imageId", adminAuth, adminController.deleteImage);
+router.get("/dashboard", adminController.dashboard);
+router.get("/admin-uploads",  adminController.adminUploads);
+router.post("/upload-image",  upload.single("image"), adminController.uploadImage);
+router.post("/delete-image/:imageId",  adminController.deleteImage);
 
 
 export default router;
