6b868446d0d8ea792eea791bd45dc586785734a4
unknown <mohameda.apxis@gmail.com>
2025-07-02 16:11:19 +0530

    dashboard design

--- src/routes/adminRouter/intex.ts
+++ src/routes/adminRouter/intex.ts
@@ -8,11 +8,11 @@ const router = Router();
 router.get("/login", adminController.login);
 router.post("/login", adminController.adminLogin);
 
-router.get("/dashboard", adminAuth, adminController.dashboard);
-router.get("/users-contacts-details", adminAuth, adminController.userContactsDetails);
-router.get("/admin-uploads", adminAuth, adminController.adminUploads);
-router.post("/upload-image", adminAuth, upload.single("image"), adminController.uploadImage);
-router.post("/delete-image/:imageId", adminAuth, adminController.deleteImage);
+router.get("/dashboard",  adminController.dashboard);
+router.get("/users-contacts-details",  adminController.userContactsDetails);
+router.get("/admin-uploads",  adminController.adminUploads);
+router.post("/upload-image",  upload.single("image"), adminController.uploadImage);
+router.post("/delete-image/:imageId",  adminController.deleteImage);
 
 
 export default router;
